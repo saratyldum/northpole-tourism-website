6d5dcb4af7d6fb8853e93251ef12a6b82579b8f5
saratyldum <98975412+saratyldum@users.noreply.github.com>
2022-11-21 14:15:44 +0100

    added javascript to hamburger icon

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import Header from './modules/header.js';
+
+Header();
---
+++ js/modules/header.js
@@ -0,0 +1,27 @@
+export default function Header() {
+	const menu = document.querySelector('.navigation__menu');
+	const menuItems = document.querySelectorAll('.navigation__menu--link');
+	const hamburgerIcon = document.querySelector('.navigation__hamburger-button');
+	const closeIcon = document.querySelector('.navigation__close-button');
+
+	hamburgerIcon.addEventListener('click', toggleMenu);
+	closeIcon.addEventListener('click', toggleMenu)
+
+	function toggleMenu() {;
+		if (menu.classList.contains('showMenu')) {
+			menu.classList.remove('showMenu');
+			closeIcon.style.display = 'none';
+		}else {
+			menu.classList.add('showMenu');
+			closeIcon.style.display = 'block'
+		}
+	}
+
+	for (let index = 0; index < menuItems.length; index +=1) {
+		menuItems[index].addEventListener('click', toggleMenu);
+	};
+
+	/**
+	 * add scroll up show navigation
+	 */
+}
