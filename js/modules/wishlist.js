1f89f10d91b89e46d5b66198bef0ffe08fa271db
saratyldum <98975412+saratyldum@users.noreply.github.com>
2022-12-06 11:02:10 +0100

    added jsdoc comments to wishlist.js

--- js/modules/wishlist.js
+++ js/modules/wishlist.js
@@ -1,5 +1,4 @@
 export default function Wishlist() {
-	// array of objects { name: "", id: "" }
 	let wishlistStorageName = "my-wishlist";
 	let wishlistItems = getWishlistFromLocal();
 	let isEditing = false;
@@ -7,29 +6,30 @@ export default function Wishlist() {
 	let isShowingAlert = false;
 	let editID = '';
 
-	/**
-	 * @TODO	rename classes that don't adhere to BEM naming system
-	 */
-
 	const alertMessage = document.querySelector('.wishlist__popup-alert');								
-	const wishlistForm = document.querySelector('.wishlist__form');
+	const wishlistSubmitButton = document.querySelector('.wishlist__form');
 	const wishlistInput = document.querySelector('#wishlist');							
 	const submitButton = document.querySelector('.wishlist__button-submit');
 	const wishlistContainer = document.querySelector('.wishlist__container');					
 	const wishlistListContainer = document.querySelector('.wishlist__list');
 	const clearButton = document.querySelector('.wishlist__button-clear');
 	const sendButton = document.querySelector('.wishlist__button-send');
-	const flyingSanta = document.querySelector('.wishlist__flying-santa');				// inconsistent
+	const flyingSanta = document.querySelector('.wishlist__flying-santa');				
 	const sentMessage = document.querySelector('.wishlist__sent');
 
 	if (wishlistListContainer !== null) {
-		wishlistForm.addEventListener('submit', handleFormSubmit);
+		wishlistSubmitButton.addEventListener('submit', handleWishlistSubmitButtonSubmit);
 		clearButton.addEventListener('click', handleClearButtonClick);
 		sendButton.addEventListener('click', handleSendButtonClick);
 	}
 
-	// handle Wish/Edit button click
-	function handleFormSubmit(event) {
+	/**
+	 * Controls what happens when the wish/edit button is clicked in the wishlist. What happens varies depending on the input and whether or not editingMode is on.
+	 * 
+	 * @param {*} event prevents default actions that belongs to the submit action in a form.
+	 * @see enableEditingMode()
+	 */
+	function handleWishlistSubmitButtonSubmit(event) {
 		event.preventDefault();
 
 		if (wishlistInput.value === '') {
@@ -48,6 +48,11 @@ export default function Wishlist() {
 		renderHTML();
 	}
 
+	/**
+	 * Controls everything that happens when the user want to delete an item from the wishlist.
+	 * 
+	 * @param {string} event Gives us the exact item that is clicked
+	 */
 	function handleDeleteButtonClick(event) {
 		const wishlistItem = event.currentTarget.parentElement.parentElement;
 		const wishlistItemID = wishlistItem.dataset.id;
@@ -58,6 +63,11 @@ export default function Wishlist() {
 		renderHTML();
 	}
 
+	/**
+	 * Controls everything that happenns when the user want to edit an item from the wishlist.
+	 * 
+	 * @param {string} event Gives us the exact item that is clicked
+	 */
 	function handleEditButtonClick(event) {
 		const wishlistItem = event.currentTarget.parentElement.parentElement;
 		const wishlistItemID = wishlistItem.dataset.id;
@@ -66,6 +76,9 @@ export default function Wishlist() {
 		renderHTML();
 	}
 
+	/**
+	 * Controls what happenns when the user want to clear the wishlist.
+	 */
 	function handleClearButtonClick() {
 		clearWishlistItems();
 		clearWishlistLocal();
@@ -73,6 +86,9 @@ export default function Wishlist() {
 		renderHTML();
 	}
 
+	/**
+	 * Controls what happenns when the user want to send the wishlist.
+	 */
 	function handleSendButtonClick() {
 		clearWishlistItems();
 		clearWishlistLocal();
@@ -80,6 +96,9 @@ export default function Wishlist() {
 		sentWishlistConfirmation();
 	}
 
+	/**
+	 * Takes the value from the input and pushes it to an array of all the wishlist items together with a unique ID saves it to local storage.
+	 */
 	function addNewWishlistItem() {
 		//creates a random string sequence to use as ID
 		const wishlistItemID = Math.random().toString(32).slice(2);
@@ -91,6 +110,9 @@ export default function Wishlist() {
 		});
 	}
 
+	/**
+	 * Finds the ID of the element that is being edited and changes its value to the new input value.
+	 */
 	function editWishlistItem() {
 		const currentlyEditingIndex = wishlistItems.findIndex(item => item.id === editID);
 		wishlistItems[currentlyEditingIndex].name = wishlistInput.value;
@@ -107,13 +129,20 @@ export default function Wishlist() {
 	}
 
 	/**
-	 * Clears all items from the wishlist and the local storage
+	 * Clears all items from the wishlist and the local storage by replacing the existing array with an empty array.
 	 */
 	function clearWishlistItems() {
 		wishlistItems = [];
 	}
 	
 
+	/**
+	 * Takes the value and ID of a wishlist item and creates the DOM-element we see on the wishlist.
+	 * 
+	 * @param {object} item The item from the wishlistItems array that needs to be created as a DOMelement
+	 * @returns itemContainer - The div with all the information and styling needed for the item to be visible in the DOM.
+	 * @see wishlistItems
+	 */
 	function returnWishlistDOMElement(item) {
 		//create wishlist DOM element
 		const itemContainer = document.createElement('div');
@@ -142,12 +171,15 @@ export default function Wishlist() {
 		return itemContainer;
 	}
 	
+	/**
+	 * The only function that makes changes to the DOM. Every action and change to the DOM will be handled with this function.
+	 */
 	function renderHTML() {
-		// reset misc values
+		// reset main values
 		wishlistListContainer.innerHTML = '';
 		wishlistInput.value = '';
 
-		// handle controls if editing a wishlist item
+		// finds the element that is being edited and puts its value in the input field and changes its value to whatever the new input is. Also changes the submit button text if item is being edited.
 		if (isEditing) {
 			const editingElement = wishlistItems.find(item => item.id === editID);
 
@@ -157,14 +189,17 @@ export default function Wishlist() {
 			submitButton.textContent = 'Wish';
 		}
 
-		// show controls only if wishlist elements exist
+		// show delete/send/item controls only if wishlist elements exist
 		if (wishlistItems.length > 0) {
 			wishlistContainer.classList.add('show-container');
 		} else {
 			wishlistContainer.classList.remove('show-container');
 		}
 
-		// show wishlist elements
+		/**
+		 * Gets the created wishlist DOM-elements from returnWishlistDOMelement() and displays them
+		 * @see returnWishlistDOMElement()
+		 */
 		for (const item of wishlistItems) {
 			const wishlistDOMElement = returnWishlistDOMElement(item);
 			wishlistListContainer.appendChild(wishlistDOMElement);
@@ -173,7 +208,7 @@ export default function Wishlist() {
 
 
 	/**
-	 * Displays an alert message on the top of the wishlist for two seconds
+	 * Displays an alert message on the top of the wishlist for two seconds.
 	 * 
 	 * @param {string} text The message to display
 	 * @param {string} action The 'success' or 'danger', each contected to a class with a different styling giving the alert a different color depending the action.
@@ -189,28 +224,36 @@ export default function Wishlist() {
 	}
 
 	/**
-	 * Edits the value of an existing item on the wishlist
+	 * Turns on editing mode when the edit button/icon is clicked
 	 * 
-	 * @param {*} event Gives us the item we want to edit
+	 * @param {string} id an unique string of number that gives us the exact item that is being edited.
 	 */
 	function enableEditingMode(id) {
 		isEditing = true;
 		editID = id;
 	}
 
+	/**
+	 * disables the editing mode when we are done editing an item.
+	 */
 	function disableEditingMode() {
 		isEditing = false;
 		editID = null;
 	}
 	
 	/**
-	 * Shows popUp modal confirming that the wishlist has been sent
+	 * Shows popUp modal confirming that the wishlist has been sent and starts the flying santa animation
 	 */
 	function sentWishlistConfirmation() {
 		sentMessage.style.display = 'block';
 		flyingSanta.classList.add('wishlist__flying-santa-animation');
 	}
 
+	/**
+	 * Sees if there are any elements in the local storage and returns them.
+	 * 
+	 * @returns either all the items that are stored in the local storage or an ampty arrays if there are no items saved yet.
+	 */
 	function getWishlistFromLocal() {
 		const storedWishlist = window.localStorage.getItem(wishlistStorageName);
 
@@ -221,13 +264,24 @@ export default function Wishlist() {
 		}
 	}
 
+	/**
+	 * Takes our array of wishlist items and pushes it to the local storage
+	 */
 	function storeWishlistLocal() {
 		window.localStorage.setItem(wishlistStorageName, JSON.stringify(wishlistItems));
 	}
 
+	/**
+	 * Clears the local storage of any data
+	 */
 	function clearWishlistLocal() {
 		window.localStorage.clear(wishlistStorageName);
 	}
+
+	/**
+	 * Runs the renderHTML function to begin with in order to get any items from the local storage and create their DOM-elements if there are any items there.
+	 * @see renderHTML()
+	 */
 	if (wishlistListContainer !== null) {
 		renderHTML();
 	}
